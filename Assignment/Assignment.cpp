6b34523e23ddafd0d8bd87bf6d1a1d163ed4e325
Jigyasu Khandelwal <jigyasu.khandelwal@research.iiit.ac.in>
2023-05-12 10:22:36 +0530

    Update Assignment.cpp

--- Assignment/Assignment.cpp
+++ Assignment/Assignment.cpp
@@ -1,43 +1,52 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+class Network {
 public:
-    int networkDelayTime(vector<vector<int>>& t, int n, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
-        vector<vector<pair<int,int>>>adj(n+1);
-        for(int i=0;i<t.size();i++){
-           adj[t[i][0]].push_back({t[i][1],t[i][2]});
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        if(times.size()<n-1)return -1;
+        vector<int>vis(n,0);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>dist(n,1e9);
+        dist[k-1]=0;
+        vector<vector<int>>adj[n];
+        for(int i=0;i<times.size();i++){
+            int u=times[i][0]-1;
+            int v=times[i][1]-1;
+            int w=times[i][2];
+            vector<int>vec;
+            vec.push_back(v);
+            vec.push_back(w);
+            adj[u].push_back(vec);
         }
-        for(int i=0;i<adj.size();i++){
-            cout<<i<<"->";
-           for(int j=0;j<adj[i].size();j++){
-               cout<<adj[i][j].first<<"-"<<adj[i][j].second<<" ";
-           }
-           cout<<endl;
-        }
-        vector<int> dist(n+1, 1e9);
-        dist[0]=0;
-        pq.push({0,k});
-        dist[k]=0;
+        pq.push({0,k-1});
+        vis[k-1]=1;
         while(!pq.empty()){
-            auto x=pq.top();
+            int d=pq.top().first;
+            int node=pq.top().second;
+            vis[node]=1;
             pq.pop();
-            int ti=x.first;
-            int node=x.second;
-            cout<<node<<" "<<ti<<" ";
-            
             for(auto it:adj[node]){
-               int adjNode = it.first;
-                int wt = it.second;
-                int curr = wt + ti;
-                if(curr<dist[adjNode])  {
-                    dist[adjNode] = curr;
-                    pq.push({curr,adjNode});
+                int edw=it[1];
+                int adjNode=it[0];
+                if(d+edw<dist[adjNode]){
+                    dist[adjNode]=d+edw;
+                    pq.push({dist[adjNode],adjNode});
                 }
             }
         }
-        // return -1;
-        if(*max_element(dist.begin(),dist.end())!=1e9)
-            return *max_element(dist.begin(),dist.end());
-        else
-        return -1;
+        int res=dist[0];
+        for(int i=0;i<n;i++){
+            res=max(res,dist[i]);
+            if(!vis[i])return -1;
+        }
+        return res;
     }
 };
+int main()
+{
+    Network S;
+    vector<vector<int>> times = {{2,1,1},{2,3,1},{3,4,1}};
+    int n = 4, k = 2;
+    int x=S.networkDelayTime(times,n,k);
+    cout<<x;
+}
